047cc1d89842efe0fd51a97c654c2844496b998c
luishsr <luishsoares@gmail.com>
2023-05-23 16:36:03 -0300

    Updates

--- src/lib.rs
+++ src/lib.rs
@@ -34,3 +34,24 @@ impl Config {
         Ok(Config { query, file_path })
     }
 }
+
+// Performs the search
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
